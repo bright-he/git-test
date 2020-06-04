5a7a06fd6134053435cbbcecde7f3a7fd51aab08
bright-he <15261802305@163.com>
2020-06-04 15:05:05 +0800

    初始化1.0版本

---
+++ package/Test1.java
@@ -0,0 +1,19 @@
+/**
+ * Test Version 1.0
+ * 这是一个测试
+ */
+public class Test1 {
+
+    private int number;
+
+    public static int version = 3.0;
+
+    public Test() {
+
+    }
+
+    public Test(String str) {
+        System.out.println(str);
+    }
+
+}
---
+++ package/readme.txt
@@ -0,0 +1,13 @@
+# 说明文件1.0版
+
+这是一个测试
+
+1 2 3
+
+int version1() {
+    // do something...
+}
+
+int version2() {
+    // do something...
+}
